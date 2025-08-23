56c1375aa315ca5a77921ee9ac4e8b0610ff3ef4
SAI GOPI A V <saiav9923@gmail.com>
2025-08-23 22:34:52 +0530

    add_multiple_conatct

--- AddressBook.java
+++ AddressBook.java
@@ -5,44 +5,46 @@ import java.util.Scanner;
 
 public class AddressBook {
 	private ArrayList<ContactPerson> contacts;
-	
-	public AddressBook()
-	{
+
+	public AddressBook() {
 		contacts = new ArrayList<ContactPerson>();
 	}
-	
-	public void addContact()
-	{
+
+	public void addContact() {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter First Name:");
-		String firstName = scanner.nextLine();
-		System.out.println("Enter Last Name:");
-		String lastName = scanner.nextLine();
-		System.out.println("Enter Address:");
-		String address = scanner.nextLine();
-		System.out.println("Enter City:");
-		String city = scanner.nextLine();
-		System.out.println("Enter State:");
-		String state = scanner.nextLine();
-		System.out.println("Enter Zip Code:");
-		String zipCode = scanner.nextLine();
-		System.out.println("Enter Phone Number:");
-		String phoneNumber = scanner.nextLine();
-		System.out.println("Enter Email:");
-		String email = scanner.nextLine();
-		contacts.add(new ContactPerson(firstName, lastName, address, city, state, zipCode,
-				phoneNumber, email));
-		System.out.println("Contact Created Successfully");
+		boolean addMore = true;
+		while (addMore) {
+			System.out.println("Enter First Name:");
+			String firstName = scanner.nextLine();
+			System.out.println("Enter Last Name:");
+			String lastName = scanner.nextLine();
+			System.out.println("Enter Address:");
+			String address = scanner.nextLine();
+			System.out.println("Enter City:");
+			String city = scanner.nextLine();
+			System.out.println("Enter State:");
+			String state = scanner.nextLine();
+			System.out.println("Enter Zip Code:");
+			String zipCode = scanner.nextLine();
+			System.out.println("Enter Phone Number:");
+			String phoneNumber = scanner.nextLine();
+			System.out.println("Enter Email:");
+			String email = scanner.nextLine();
+			contacts.add(new ContactPerson(firstName, lastName, address, city, state, zipCode, phoneNumber, email));
+			System.out.println("Contact Created Successfully");
+			System.out.println("Do you want to add another contact(yes/no)");
+			String option = scanner.nextLine();
+			if (!option.equalsIgnoreCase("yes")) {
+				addMore = false;
+			}
+		}
 
 	}
-	
-	public void editContact(String name)
-	{
+
+	public void editContact(String name) {
 		Scanner scanner = new Scanner(System.in);
-		for(ContactPerson contact :contacts)
-		{
-			if(contact.getFirstName().equalsIgnoreCase(name))
-			{
+		for (ContactPerson contact : contacts) {
+			if (contact.getFirstName().equalsIgnoreCase(name)) {
 				System.out.println("Contact Found.Enter new details");
 				System.out.println("Enter Address:");
 				contact.setAddress(scanner.nextLine());
@@ -56,27 +58,22 @@ public class AddressBook {
 				contact.setPhoneNumber(scanner.nextLine());
 				System.out.println("Enter Email:");
 				contact.setEmail(scanner.nextLine());
-			}else {
+			} else {
 				System.out.println("Contact Not Found..");
 			}
 		}
 	}
-	
-	public void deleteContact(String name)
-	{
-		for(ContactPerson contact : contacts)
-		{
-			if(contact.getFirstName().equalsIgnoreCase(name)) {
+
+	public void deleteContact(String name) {
+		for (ContactPerson contact : contacts) {
+			if (contact.getFirstName().equalsIgnoreCase(name)) {
 				contacts.remove(contact);
 				System.out.println("Contact Deleted Successfully");
 				return;
-			}
-			else {
+			} else {
 				System.out.println("Contact Not Found");
 			}
 		}
 	}
 
-
-	
 }
